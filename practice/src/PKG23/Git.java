8d80a42266565a224a3bf9cdb3efa6fce1300a61
Gaurav.Sharma <Gaurav.Sharma@Gaurav-5CD2306195.znl.com>
2023-01-27 23:06:33 +0530

    i m your first message

---
+++ practice/src/PKG23/Git.java
@@ -0,0 +1,11 @@
+package PKG23;
+
+public class Git {
+
+	public static void main(String[] args)
+	{
+		System.out.println("i m your git commend");
+
+	}
+
+}
---
+++ practice/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Gaurav.Sharma
+ *
+ */
+module practice {
+}
